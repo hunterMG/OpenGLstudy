c9c408d5d5bd205cd0884c6e56591e4b79a25dcb
Guang Yan. <vyg178@163.com>
2017-05-03 22:57:56 +0800

    CG05: finished

--- CG05/CG05.cpp
+++ CG05/CG05.cpp
@@ -2,13 +2,11 @@
 //http://www.tuicool.com/articles/VRRVV3j hide the console window
 #include <GL\glew.h>
 #include <GL\glut.h>
-//#include <GL/glaux.h>
+#include <GL/glaux.h>
 #include <time.h>
 #include <math.h>
 #include <iostream>
-#include <tchar.h>
 #include <stdlib.h>
-#include <windows.h>
 #include "tgaload.h"
 GLfloat angle = 0.0; //set the angle of rotation 
 
@@ -27,19 +25,51 @@ void init()
 {
 	glShadeModel(GL_SMOOTH);							// Enable Smooth Shading
 	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);				// Black Background
-	glEnable(GL_COLOR_MATERIAL);
-	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
-
+	glEnable(GL_COLOR_MATERIAL);//启用材质
+	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);//设置材质
+	//纹理贴图
 	glEnable(GL_TEXTURE_2D);
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 	glGenTextures(1, texture_id);
-
 	image_t   temp_image;
-
 	glBindTexture(GL_TEXTURE_2D, texture_id[CUBE_TEXTURE]);
-	//tgaLoad("swirl.tga", &temp_image, TGA_FREE | TGA_LOW_QUALITY);
-	tgaLoad("chuyin2.tga", &temp_image, TGA_FREE | TGA_LOW_QUALITY);
+	tgaLoad("chuyin2.tga", &temp_image, TGA_FREE | TGA_LOW_QUALITY);//加载图片
 	glEnable(GL_CULL_FACE);
+	/*
+	//光源1 绿色
+	GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.1 };
+	GLfloat light_color[] = { 0.0, 1.0, 0.0, 1.0 };
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_color);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_color);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_color);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	//光源2 聚光光源 红色
+	*/
+	GLfloat mat_ambient[] = { 0.2, 0.2, 0.2, 1.0 }; GLfloat mat_diffuse[] = { 0.8, 0.8, 0.8, 1.0 };
+	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 }; GLfloat mat_shininess[] = { 50.0 };
+	GLfloat light0_diffuse[] = { 0.0, 0.0, 1.0, 1.0 }; GLfloat light0_position[] = { 3.0, 3.0, 1.0, 0.0 };
+	GLfloat light1_ambient[] = { 0.2, 0.2, 0.2, 1.0 }; GLfloat light1_diffuse[] = { 1.0, 0.0, 0.0, 1.0 };
+	GLfloat light1_specular[] = { 1.0, 0.6, 0.6, 1.0 }; GLfloat light1_position[] = { -3.0, -3.0, 1.0, 1.0 };
+	GLfloat spot_direction[] = { 1.0,1.0,-1.0 };
+	glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);//材质--反射什么光--决定物体表现的颜色
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);//材质的镜面指数
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse);//RGBA模式下漫反射光
+	glLightfv(GL_LIGHT0, GL_POSITION, light0_position);//光源位置齐次坐标
+	glLightfv(GL_LIGHT1, GL_AMBIENT, light1_ambient);//RGBA模式下环境光
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, light1_diffuse);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, light1_specular);//RGBA模式下镜面光
+	glLightfv(GL_LIGHT1, GL_POSITION, light1_position);
+	glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, 30.0);//点光源聚光截止角
+	glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, spot_direction);//点光源聚光方向矢量
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHT1);
+	glDepthFunc(GL_LESS);
+	glEnable(GL_DEPTH_TEST);
 }
 void draw1(GLdouble dSize)
 {
@@ -125,7 +155,7 @@ void polygon(int a, int b, int c, int d,int color)
 	glVertex3fv(vertices[c]); glVertex3fv(vertices[d]);
 	glEnd();
 }
-void trianle(int a, int b, int c, int color)
+void triangle(int a, int b, int c, int color)
 {
 	glBegin(GL_POLYGON); glColor3fv(colors[color]);
 	glTexCoord2f(0.0f, 1.0f); glVertex3fv(point[a]);
@@ -139,38 +169,55 @@ void colorcube()
 	polygon(1, 2, 6, 5, 4); polygon(4, 5, 6, 7, 5); polygon(0, 1, 5, 4, 6);
 }
 void colorPyramid() {
-	trianle(0, 1, 2, 6); trianle(0, 3, 1, 6);
-	trianle(0, 2, 3, 1); trianle(1, 2, 3, 6);
+	triangle(0, 1, 2, 6); //6为白色，使其表面仅为图片颜色，反应光照颜色 
+	triangle(0, 3, 1, 6); //前三个数的顺序不可错，否则看不到图片
+	triangle(0, 2, 3, 6); 
+	triangle(1, 3, 2, 6); 
 }
 GLfloat m_viewer[3] = { 0,0,6 };
 GLfloat m_theta[3] = { 0,0,0 };
 void display1()
 {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	//更新视点位置
+	//glTranslatef(0, 0, -5);
 	gluLookAt(m_viewer[0], m_viewer[1], m_viewer[2], 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-	glRotatef(m_theta[0], 1.0, 0.0, 0.0);
-	glRotatef(m_theta[1], 0.0, 1.0, 0.0); /* 旋转立方体 */
-	glRotatef(m_theta[2], 0.0, 0.0, 1.0);
-
+	//        眼睛的位置                              观察方向        头向上的方向
+	/*
+	glTranslatef()采用模型变换----移动物体
+	gluLookAt()采用视图变换-------移动照相机（眼睛）
+	-----效果一样
+	*/
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glEnable(GL_DEPTH_TEST);//深度测试消除隐藏面 
-	//colorcube();
-	//glBindTexture(GL_TEXTURE_2D, texture_id[0]);
+	
+	glBindTexture(GL_TEXTURE_2D, texture_id[0]);
+	
+	glPushMatrix();
+	//glLoadIdentity();
+	glTranslated(-3.0, -3.0, 1.0);
+	glDisable(GL_LIGHTING);
+	glColor3f(1.0, 0.0, 0.0);
+	glutWireCube(0.1);//标注光源1位置--红
+	glEnable(GL_LIGHTING);
+	glPopMatrix();
+	
+	glPushMatrix();
+	glTranslated(3.0, 3.0, 1.0);
+	glColor3f(0.0, 0.0, 1.0);
+	glutWireCube(0.1);//标注光源0位置--蓝
+	glPopMatrix();
+
+	glRotatef(m_theta[0], 1.0, 0.0, 0.0);
+	glRotatef(m_theta[1], 0.0, 1.0, 0.0); // 旋转 
+	glRotatef(m_theta[2], 0.0, 0.0, 1.0);
+
 	colorPyramid();
 	glutSwapBuffers();
-	//m_theta[0] += 0.01f;
-	m_theta[1] += 0.03f;
-	//m_theta[2] += 0.01f;
-}
-
-void timer(int value)
-{
-	//glutPostRedisplay();
-	display();
-	
-	glutTimerFunc(50, timer, 0);
+	m_theta[0] += 1.0f;
+	m_theta[1] += 1.0f;
+	m_theta[2] += 1.0f;
 }
 void keyboard(unsigned char key, int x, int y)
 {
@@ -181,19 +228,6 @@ void keyboard(unsigned char key, int x, int y)
 	default:
 		break;
 	}
-}
-void reshape(int width, int height)
-{
-	glViewport(0, 0, width, height);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	//glOrtho(-width / 2, width / 2, -height / 2, height / 2, 0, 10);
-	//glMatrixMode(GL_MODELVIEW);
-	//glLoadIdentity();
-	gluPerspective(60, (GLfloat)width / (GLfloat)height, 1.0, 10.0);//透视投影
-	
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
 }
 void display2(void)
 {
@@ -246,10 +280,10 @@ void display2(void)
 	glutSwapBuffers();
 }
 
-void reshape2(int w, int h)
+void reshape(int w, int h)
 {
 	// Prevent a divide by zero, when window is too short
-	// (you cant make a window of zero width).
+	// (you can't make a window of zero width).
 	if (h == 0)
 		h = 1;
 
@@ -257,17 +291,17 @@ void reshape2(int w, int h)
 	// Reset the coordinate system before modifying
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-
 	// Set the viewport to be the entire window
 	glViewport(0, 0, w, h);
-
 	// Set the clipping volume
-	gluPerspective(80, ratio, 1, 200);
+	gluPerspective(80, ratio, 1, 200);//透视投影
+	//glOrtho(-width / 2, width / 2, -height / 2, height / 2, 0, 10);
+	//glMatrixMode(GL_MODELVIEW);
+	//glLoadIdentity();
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(0, 0, 30,
-		0, 0, 10,
-		0.0f, 1.0f, 0.0f);
+	//gluLookAt(0, 0, 10, 	0, 0, 10,    0.0f, 1.0f, 0.0f);
+	gluLookAt(m_viewer[0], m_viewer[1], m_viewer[2], 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 }
 void arrow_keys(int a_keys, int x, int y)  // Create Special Function (required for arrow keys)
 {
@@ -282,6 +316,13 @@ void arrow_keys(int a_keys, int x, int y)  // Create Special Function (required 
 		break;
 	}
 }
+void timer(int value)
+{
+	//glutPostRedisplay();
+	display1();
+
+	glutTimerFunc(10, timer, 0);
+}
 int main(int argc, char**argv)
 {
 	glutInit(&argc, argv);
@@ -292,9 +333,9 @@ int main(int argc, char**argv)
 	//glewInit();
 	init();
 	glutDisplayFunc(display1);//窗口显示后调用,WM_PAINT  
-	glutIdleFunc(display1);//空闲时调用
-	//glutTimerFunc(50, timer, 0);
-	glutReshapeFunc(reshape2);
+	//glutIdleFunc(display1);//空闲时调用
+	glutTimerFunc(50, timer, 0);
+	glutReshapeFunc(reshape);
 	glutKeyboardFunc(keyboard);
 	glutSpecialFunc(arrow_keys);
 	glutMainLoop();
---
+++ test/test.cpp
@@ -0,0 +1,43 @@
+#include <GL/glut.h>  
+#include <stdlib.h>  
+
+void init(void)
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0); //±³¾°ºÚÉ«  
+}
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 1.0, 1.0); //»­±Ê°×É«  
+
+	glLoadIdentity();  //¼ÓÔØµ¥Î»¾ØÕó  
+	//glTranslatef(0, 0, -5);
+	gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	glutWireTeapot(2);
+	glutSwapBuffers();
+}
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60.0, (GLfloat)w / (GLfloat)h, 1.0, 20.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+}
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow(argv[0]);
+	init();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMainLoop();
+	return 0;
+}
